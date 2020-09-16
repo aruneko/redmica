65768a610b124d29d915660f923c31ac33ab9d4c
Aruneko <aruneko99@gmail.com>
2020-09-16 11:01:08 +0900

    add log settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,4 +24,24 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  # Logging by JSON
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  config.lograge.enabled = true
+  config.lograge.formatter = Lograge::Formatters::Json.new
+  config.lograge.custom_payload do |controller|
+    current_user = controller.find_current_user
+    user_name = current_user&.logged? ? "#{current_user.login}" : "anonymous"
+    user_id = current_user&.logged? ? "#{current_user.id}" : "0"
+    {
+      remote_ip: controller.request.remote_ip,
+      user_name: user_name,
+      user_id: user_id
+    }
+  end
 end
